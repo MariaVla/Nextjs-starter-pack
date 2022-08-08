0c528f8f87ce031f2afbf67ec2f93cfa57e0f622
Dev <dev@Majos-MacBook-Air.local>
2022-08-08 10:17:14 +0200

    Add more links navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,34 @@ import Link from 'next/link';
 
 export const Navbar = () => {
   return (
-    <>
-      <nav>
-        <Link href="/login">
-          <a>
-            <span>Login</span>
-          </a>
-        </Link>
-      </nav>
-    </>
+    <nav style={{ padding: '2rem' }}>
+      <Link href="/">
+        <a>
+          <span>Home</span>
+        </a>
+      </Link>
+
+      <Link href="/login">
+        <a>
+          <span>Login</span>
+        </a>
+      </Link>
+
+      <Link href="/about">
+        <a>
+          <span>About</span>
+        </a>
+      </Link>
+      <Link href="/users">
+        <a>
+          <span>Users</span>
+        </a>
+      </Link>
+      <Link href="/friends">
+        <a>
+          <span>Friends</span>
+        </a>
+      </Link>
+    </nav>
   );
 };
