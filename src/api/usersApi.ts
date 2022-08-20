3cc1b58c24132e2429e6d8c911ef8964b94825ca
MariaVla <majo.vlasanovich@gmail.com>
2022-08-20 16:06:45 +0200

    Develop (#3)
    * Change API url to Heroku fake json server
    
    Co-authored-by: Dev <dev@Majos-MacBook-Air.local>

--- src/api/usersApi.ts
+++ src/api/usersApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const usersApi = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'https://my-fake-json-server.herokuapp.com',
 });
 
 export default usersApi;
