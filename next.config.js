aaac7ff604a05562d2d5627aad33fa72ab5fb786
MariaVla <majo.vlasanovich@gmail.com>
2022-08-22 16:27:04 +0200

    I18n (#4)
    * Setup i18n
    
    * Default spanish
    
    Co-authored-by: Dev <dev@Majos-MacBook-Air.local>

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  i18n: {
+    locales: ['es', 'en'],
+    defaultLocale: 'es',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
