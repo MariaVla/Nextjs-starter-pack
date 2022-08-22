3382a4d773ac97e766a9ef75bc85ef2a05a8133f
Dev <dev@Majos-MacBook-Air.local>
2022-08-22 11:15:07 -0300

    Setup i18n

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+
+import { useRouter } from 'next/router';
+import { IntlProvider } from 'react-intl';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import en from '../src/lang/en.json';
+import es from '../src/lang/es.json';
+
 import '../styles/globals.css';
+
+const messages = {
+  en,
+  es,
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = React.useState(() => new QueryClient());
 
+  const { locale } = useRouter();
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <IntlProvider locale={locale} messages={messages[locale]}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </IntlProvider>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,47 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+import { FormattedMessage, useIntl } from 'react-intl';
+
 import { MainLayout } from '../src/components/layouts/MainLayout';
+
 import styles from '../styles/Common.module.css';
 
 const HomePage: NextPage = () => {
+  const { locales } = useRouter();
+
+  const intl = useIntl();
+
+  const title = intl.formatMessage({ id: 'page.home.head.title' });
+  const description = intl.formatMessage({
+    id: 'page.home.head.meta.description',
+  });
+
   return (
-    <MainLayout
-      title={'Home'}
-      description="Home Page description"
-      content={'Home Page'}
-    >
-      <h1 className={styles.title}>Home Page</h1>
-      <p className={styles.description}>Get started!</p>
+    <MainLayout title={title} description={description} content={'Home Page'}>
+      <header>
+        <div className={styles.languages}>
+          {[...locales].sort().map((locale) => (
+            <Link key={locale} href="/" locale={locale}>
+              {locale}
+            </Link>
+          ))}
+        </div>
+      </header>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          <FormattedMessage
+            id="page.home.title"
+            values={{ b: (chunks) => <b>{chunks}</b> }}
+          />
+        </h1>
+
+        <p className={styles.description}>
+          <FormattedMessage id="page.home.description" />
+        </p>
+      </main>
     </MainLayout>
   );
 };
